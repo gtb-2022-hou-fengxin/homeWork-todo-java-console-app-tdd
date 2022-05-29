7679ae435285da66d54585a74729ae28a773c758
Houfengxin <18192062162@163.com>
2022-05-29 17:31:37 +0800

    [hfx] chore: complete the existing task list tests

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,8 +1,22 @@
 package com.tw.cn.cap.gtb.todo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
         throw new UnsupportedOperationException();
+    }
+
+    public List<String> run() {
+//        return List.of("Task 01","Task 02","Task 03");
+        try {
+            return Files.readAllLines(Path.of("/c/Users/86130/.todo/tasks"));
+        } catch (IOException e) {
+            throw new TodoCannotReadFileException();
+        }
     }
 }
--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -1,9 +1,16 @@
 package com.tw.cn.cap.gtb.todo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 
 class AppTest {
 
+    @Test
+    void should_list_existing_tasks(){
+        final var result = new App().run();
+        Assertions.assertEquals(List.of("Task 01","Task 02","Task 03"),result);
+    }
 }
