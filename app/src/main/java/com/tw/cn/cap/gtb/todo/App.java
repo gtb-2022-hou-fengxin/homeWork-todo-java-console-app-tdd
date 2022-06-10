f387faf8019302d40d5a1d15646a634ba9f72be2
Houfengxin <18192062162@163.com>
2022-06-10 16:42:10 +0800

    [hfx]:add a new task with single word as name

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,5 +1,9 @@
 package com.tw.cn.cap.gtb.todo;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class App {
@@ -7,13 +11,24 @@ public class App {
     private final ListCommand listCommand = new ListCommand();
 
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        new App().run().forEach(System.out::println);
     }
 
-    public List<String> run() {
+    public List<String> run(String... args) {
+        if(args.length > 0 && args[0].equals("add")){
+            try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH, StandardOpenOption.APPEND)) {
+                final var taskName =args[1];
+                bw.write("+ " + taskName);
+//                bw.newLine();
+            } catch (IOException e) {
+//                将其改成ToDoException
+                throw new TodoCannotReadFileException();
+            }
+            return List.of();
+        }
         return listCommand.run();
     }
-
 
 
 }
--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -21,5 +21,20 @@ class AppTest {
                 "4 Task 04",
                 "5 Task 05"),result);
     }
+
+    @Test
+    void should_add_task_single_word_as_name(){
+        new App().run("add","foobar");
+        final var result = new App().run();
+        Assertions.assertEquals(List.of(
+                "# To be done",
+                "1 Task 01",
+                "2 Task 02",
+                "3 Task 03",
+                "6 foobar",
+                "# Completed",
+                "4 Task 04",
+                "5 Task 05"),result);
+    }
     //integration test
 }
