b555ad669ea0f142ba509fe57009b6714b6c334f
Houfengxin <18192062162@163.com>
2022-06-10 18:24:17 +0800

    [hfx] refactor: extract AddCommand and extract app field in AppTest

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,9 +1,5 @@
 package com.tw.cn.cap.gtb.todo;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class App {
@@ -17,23 +13,10 @@ public class App {
 
     public List<String> run(String... args) {
         if(args.length > 0 && args[0].equals("add")){
-            return execute(args);
+            return new Addcommand(args).execute();
         }else {
             return listCommand.run();
         }
-    }
-
-    private List<String> execute(String[] args) {
-        try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH, StandardOpenOption.APPEND)) {
-            //ctrl+shift+v 提取变量，并shift+alt+↑
-            final var taskName = args[1];
-            bw.write("+ " + taskName);
-//                bw.newLine();
-        } catch (IOException e) {
-//                将其改成ToDoException
-            throw new TodoCannotReadFileException();
-        }
-        return List.of();
     }
 
 
--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 class AppTest {
 
+    private App app;
+
     @BeforeEach
     void setUp(){
         writeDataFile(List.of(
@@ -20,6 +22,7 @@ class AppTest {
                 "+ Task 03",
                 "* Task 04",
                 "* Task 05"));
+        app = new App();
     }
 
     @Nested
@@ -28,7 +31,10 @@ class AppTest {
         class WhenThereAreExistingTasks{
             @Test
             void should_list_existing_tasks(){
-                final var result = new App().run();
+                //Given
+                //When
+                final var result = app.run();
+                //Then
                 Assertions.assertEquals(List.of(
                         "# To be done",
                         "1 Task 01",
@@ -49,8 +55,10 @@ class AppTest {
         class WhenSingleWordProvidedAsName{
             @Test
             void should_add_task_single_word_as_name(){
-                new App().run("add","foobar");
-                final var result = new App().run();
+                //ctrl +alt + T提取变量
+//                final var app =new App();
+                app.run("add","foobar");
+                final var result = app.run();
                 Assertions.assertEquals(List.of(
                         "# To be done",
                         "1 Task 01",
