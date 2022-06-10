3093aa296894a437c8e4ca5d5c9c1ebf43fa21f2
Houfengxin <18192062162@163.com>
2022-06-10 17:27:57 +0800

    [hfx] test:make AppTest can run repeatable

--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -2,11 +2,11 @@ package com.tw.cn.cap.gtb.todo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 
@@ -22,32 +22,47 @@ class AppTest {
                 "* Task 05"));
     }
 
-    @Test
-    void should_list_existing_tasks(){
-        final var result = new App().run();
-        Assertions.assertEquals(List.of(
-                "# To be done",
-                "1 Task 01",
-                "2 Task 02",
-                "3 Task 03",
-                "# Completed",
-                "4 Task 04",
-                "5 Task 05"),result);
+    @Nested
+     class ListCommand{
+        @Nested
+        class WhenThereAreExistingTasks{
+            @Test
+            void should_list_existing_tasks(){
+                final var result = new App().run();
+                Assertions.assertEquals(List.of(
+                        "# To be done",
+                        "1 Task 01",
+                        "2 Task 02",
+                        "3 Task 03",
+                        "# Completed",
+                        "4 Task 04",
+                        "5 Task 05"),result);
+            }
+
+        }
+
     }
 
-    @Test
-    void should_add_task_single_word_as_name(){
-        new App().run("add","foobar");
-        final var result = new App().run();
-        Assertions.assertEquals(List.of(
-                "# To be done",
-                "1 Task 01",
-                "2 Task 02",
-                "3 Task 03",
-                "6 foobar",
-                "# Completed",
-                "4 Task 04",
-                "5 Task 05"),result);
+    @Nested
+    class AddCommand{
+        @Nested
+        class WhenSingleWordProvidedAsName{
+            @Test
+            void should_add_task_single_word_as_name(){
+                new App().run("add","foobar");
+                final var result = new App().run();
+                Assertions.assertEquals(List.of(
+                        "# To be done",
+                        "1 Task 01",
+                        "2 Task 02",
+                        "3 Task 03",
+                        "6 foobar",
+                        "# Completed",
+                        "4 Task 04",
+                        "5 Task 05"),result);
+            }
+        }
+
     }
 
     private void writeDataFile(List<String> lines) {
